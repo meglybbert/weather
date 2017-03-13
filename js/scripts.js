c3b630cc2bae00e1eb35f7ee20b767879ca31658
meglybbert <mlybbert1@vcd-ceb131-18.eastern.ewu.edu>
2017-03-13 09:38:43 -0700

    Brought in code from codepen
    Updated html, css and js

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,32 @@
 // YOUR SCRIPTS GO HERE
+$.simpleWeather({
+  location: 98926,
+  unit: 'f',
+  success: function(weather){
+    //weather check
+    console.log(weather)
+    //display weather
+    $('.city').text(weather.city);
+    $('.state').text(weather.region);
+    $('.temp').text(weather.temp);
+    $('.thumb img').attr('src', weather.image);
+  },
+  error: function(error){
+    console.log('No Weather Found');
+  }
+});$.simpleWeather({
+  location: 98926,
+  unit: 'f',
+  success: function(weather){
+    //weather check
+    console.log(weather)
+    //display weather
+    $('.city').text(weather.city);
+    $('.state').text(weather.region);
+    $('.temp').text(weather.temp);
+    $('.thumb img').attr('src', weather.image);
+  },
+  error: function(error){
+    console.log('No Weather Found');
+  }
+});
